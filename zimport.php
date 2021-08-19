157e5026526ad448a3de2ee14d996f7c793396c6
zapalm <zapalm@ya.ru>
2021-08-19 10:28:03 +1000

    Added an integration with Quality service; made some code improvements; added PHPDoc

--- zimport.php
+++ zimport.php
@@ -14,7 +14,10 @@ if (!defined('_CAN_LOAD_FILES_')) {
 
 class ZImport extends Module
 {
-    /** @var string наименование таба модуля */
+    /** The product ID of the module on its homepage. */
+    const HOMEPAGE_PRODUCT_ID = 14;
+
+    /** @var string The module's tab name. */
     private $module_tab;
 
     function __construct()
@@ -30,30 +33,66 @@ class ZImport extends Module
         $this->description = $this->l('Enhanced import tool, that supports Excel files.');
     }
 
+    /**
+     * @inheritDoc
+     *
+     * @author Maksim T. <zapalm@yandex.com>
+     */
     public function install()
     {
-        return $this->installModuleTab($this->module_tab, 'AdminTools') && parent::install();
+        $result = (bool)parent::install();
+
+        if ($result) {
+            $result = $this->installModuleTab($this->module_tab, 'AdminTools');
+        }
+
+        $this->registerModuleOnQualityService('installation');
+
+        return $result;
     }
 
+    /**
+     * @inheritDoc
+     *
+     * @author Maksim T. <zapalm@yandex.com>
+     */
     public function uninstall()
     {
-        return $this->uninstallModuleTab($this->module_tab) && parent::uninstall();
+        $result = (bool)parent::uninstall();
+
+        if ($result) {
+            $result = $this->uninstallModuleTab($this->module_tab);
+        }
+
+        $this->registerModuleOnQualityService('uninstallation');
+
+        return $result;
     }
 
+    /**
+     * Installs a tab of the module.
+     *
+     * @param string $tab_name
+     * @param string $parent_tab_name
+     *
+     * @return bool
+     *
+     * @author Maksim T. <zapalm@yandex.com>
+     */
     private function installModuleTab($tab_name, $parent_tab_name)
     {
         @copy(_PS_MODULE_DIR_ . $this->name . '/zimportadmin.gif', _PS_IMG_DIR_ . 't/' . $tab_name . '.gif');
         $tab = new Tab();
 
-        // subtab name in different languages
-        $langs = Language::getLanguages();
-        foreach ($langs as $l) {
+        // Sub-tab name in different languages
+        $languages = Language::getLanguages();
+        foreach ($languages as $l) {
             switch ($l['iso_code']) {
                 case 'ru':
                     $tab->name[$l['id_lang']] = 'Импорт+';
                     break;
                 case 'fr':
-                    $tab->name[$l['id_lang']] = 'Import+';
+                    $tab->name[$l['id_lang']] = 'Importer+';
                     break;
                 case 'es':
                     $tab->name[$l['id_lang']] = 'Importar+';
@@ -65,21 +104,30 @@ class ZImport extends Module
         }
 
         $tab->class_name = $tab_name;
-        $tab->module = $this->name;
+        $tab->module     = $this->name;
+        $tab->id_parent  = Tab::getIdFromClassName($parent_tab_name);
 
-        $id_parent = Tab::getIdFromClassName($parent_tab_name);
-        $tab->id_parent = $id_parent;
-
-        return $tab->save();
+        return (bool)$tab->save();
     }
 
+    /**
+     * Uninstalls a tab of the module.
+     *
+     * @param string $tab_name
+     *
+     * @return bool
+     *
+     * @author Maksim T. <zapalm@yandex.com>
+     */
     private function uninstallModuleTab($tab_name)
     {
         $idTab = Tab::getIdFromClassName($tab_name);
-        if ($idTab != 0) {
-            $tab = new Tab($idTab);
-            $tab->delete();
+        if ($idTab > 0) {
             @unlink(_PS_IMG_DIR_ . 't/' . $tab_name . '.gif');
+
+            $tab = new Tab($idTab);
+
+            return (bool)$tab->delete();
         }
 
         return true;
@@ -103,5 +151,35 @@ class ZImport extends Module
         ';
 
         return $output;
+    }
+
+    /**
+     * Registers current module installation/uninstallation in the quality service.
+     *
+     * This method is needed for a developer to quickly find out about a problem with installing or uninstalling a module.
+     *
+     * @param string $operation The operation. Possible values: installation, uninstallation.
+     *
+     * @author Maksim T. <zapalm@yandex.com>
+     */
+    private function registerModuleOnQualityService($operation)
+    {
+        @file_get_contents('https://prestashop.modulez.ru/scripts/quality-service/index.php?' . http_build_query([
+            'data' => json_encode([
+                'productId'           => self::HOMEPAGE_PRODUCT_ID,
+                'productSymbolicName' => $this->name,
+                'productVersion'      => $this->version,
+                'operation'           => $operation,
+                'status'              => (empty($this->_errors) ? 'success' : 'error'),
+                'message'             => (false === empty($this->_errors) ? strip_tags(stripslashes(implode(' ', (array)$this->_errors))) : ''),
+                'prestashopVersion'   => _PS_VERSION_,
+                'thirtybeesVersion'   => (defined('_TB_VERSION_') ? _TB_VERSION_ : ''),
+                'shopDomain'          => (method_exists('Tools', 'getShopDomain') && Tools::getShopDomain() ? Tools::getShopDomain() : (Configuration::get('PS_SHOP_DOMAIN') ? Configuration::get('PS_SHOP_DOMAIN') : Tools::getHttpHost())),
+                'shopEmail'           => Configuration::get('PS_SHOP_EMAIL'), // This public e-mail from a shop's contacts can be used by a developer to send only an urgent information about security issue of a module!
+                'phpVersion'          => PHP_VERSION,
+                'ioncubeVersion'      => (function_exists('ioncube_loader_iversion') ? ioncube_loader_iversion() : ''),
+                'languageIsoCode'     => Language::getIsoById(false === empty($GLOBALS['cookie']->id_lang) ? $GLOBALS['cookie']->id_lang : Context::getContext()->language->id),
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE),
+        ]));
     }
 }
